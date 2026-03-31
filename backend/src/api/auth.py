dd6f0883b791b7acb2a2ed47e5f0b8a376c97ff2
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-04-01 00:40:44 +0700

    add generate code for mail

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -1,4 +1,5 @@
 import os
+import secrets
 from datetime import datetime, timedelta
 from typing import Optional
 
@@ -7,12 +8,14 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 
-from backend.src.api.database import get_db
-from backend.src.api.crud import get_user_by_email, get_user_by_username
-from backend.src.api.models import User
-from backend.src.api.schemas import TokenData
+from database import get_db
+# from crud import get_user_by_email, get_user_by_username
+from models import User
+from schemas import TokenData
 
 
 load_dotenv()
@@ -23,6 +26,13 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def generate_code():
+    return secrets.randbelow(1000000)
+
+async def get_user_by_username(db: AsyncSession, username: str):
+    result = await db.execute(select(User).where(User.username == username))
+    return result.scalar_one_or_none()
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -56,7 +66,7 @@ async def authenticate_user(fake_db, username: str, password: str):
     return user
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="not validate data",
