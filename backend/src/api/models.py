bd14a99f5e7c91a5830956e9b3c9f57f341ce5e4
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-04-14 01:51:59 +0700

    fix structure

--- backend/src/api/models.py
+++ backend/src/api/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Float, Text
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Float, Text, func, text, true, false
 from sqlalchemy.orm import relationship, Mapped
 
 from database import Base
@@ -14,12 +14,12 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     username = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=True)
-    is_active = Column(Boolean, default=True)
-    is_verified = Column(Boolean, default=False)
+    is_active = Column(Boolean, server_default=true())
+    is_verified = Column(Boolean, server_default=false())
     verification_code = Column(Integer, nullable=True)
     code_expires_at = Column(DateTime, nullable=True)
     google_id = Column(String, unique=True, nullable=True)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, server_default=func.now())
 
     boards = relationship("Board", back_populates="owner", cascade="all, delete-orphan")
     pins = relationship("Pin", back_populates="author", cascade="all, delete-orphan")
@@ -32,13 +32,13 @@ class Board(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
-    is_private = Column(Boolean, default=False)
+    is_private = Column(Boolean, server_default=false())
 
     user_id = Column(Integer, ForeignKey("web_users.id", ondelete="CASCADE"), nullable=False)
     owner = relationship("User", back_populates="boards")
     pins = relationship("Pin", back_populates="board", cascade="all, delete-orphan")
 
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, server_default=func.now())
 
 class Pin(Base):
     __tablename__ = "pins"
@@ -53,12 +53,12 @@ class Pin(Base):
     # image_height = Column(Integer, default=0)
     # aspect_ratio = Column(Float, default=1.0)
 
-    likes_count = Column(Integer, default=0, nullable=False)
+    likes_count = Column(Integer, server_default=text("0"), nullable=False)
     # saves_count = Column(Integer, default=0)
 
     user_id = Column(Integer, ForeignKey("web_users.id", ondelete="CASCADE"), nullable=False)
     board_id = Column(Integer, ForeignKey("boards.id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow, index=True)
+    created_at = Column(DateTime, server_default=func.now(), index=True)
 
     author: Mapped["User"] = relationship("User", back_populates="pins")
     board = relationship("Board", back_populates="pins")
@@ -67,11 +67,11 @@ class Pin(Base):
 
 
 class PinLike(Base):
-    __tablename__ = "pin_lakes"
+    __tablename__ = "pin_likes"
     id = Column(Integer, primary_key=True, index=True)
     pin_id = Column(Integer, ForeignKey("pins.id", ondelete="CASCADE"), nullable=False)
     user_id = Column(Integer, ForeignKey("web_users.id", ondelete="CASCADE"), nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, server_default=func.now())
 
     pin = relationship("Pin", back_populates="likes")
     user = relationship("User", back_populates="pin_likes")
@@ -82,7 +82,7 @@ class PinComments(Base):
     pin_id = Column(Integer, ForeignKey("pins.id", ondelete="CASCADE"), nullable=False)
     user_id = Column(Integer, ForeignKey("web_users.id", ondelete="CASCADE"), nullable=False)
     comment = Column(Text, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, server_default=func.now())
 
     pin = relationship("Pin", back_populates="comments")
     user = relationship("User", back_populates="comments")
